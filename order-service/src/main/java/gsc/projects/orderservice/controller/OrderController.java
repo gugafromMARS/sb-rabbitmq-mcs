d63009c875292b432ea378014aa554684c90cdfc
Gonçalo <goncaloscravo@gmail.com>
2023-11-06 15:30:49 +0000

    Add: controller issue

--- order-service/src/main/java/gsc/projects/orderservice/controller/OrderController.java
+++ order-service/src/main/java/gsc/projects/orderservice/controller/OrderController.java
@@ -6,10 +6,7 @@ import gsc.projects.orderservice.service.OrderService;
 import gsc.projects.orderservice.service.OrderServiceImp;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -19,7 +16,7 @@ public class OrderController {
 
     private OrderService orderService;
 
-    @GetMapping("/publish")
+    @PostMapping("/publish")
     public ResponseEntity<?> send(@RequestBody Order order){
         orderService.sendOrder(order);
         return ResponseEntity.ok("Order sent successfully");
