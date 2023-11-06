d5ac937065a85791b998db4f4e43e2c827d28293
Gonçalo <goncaloscravo@gmail.com>
2023-11-06 15:05:09 +0000

    Add: email order consumer implementation

---
+++ email-service/src/main/java/gsc/projects/emailservice/dto/Order.java
@@ -0,0 +1,29 @@
+package gsc.projects.emailservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+
+    private String orderId;
+    private String name;
+    private int quantity;
+    private double price;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
